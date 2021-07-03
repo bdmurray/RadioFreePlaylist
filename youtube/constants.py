0aa601844c3b3a5fe9780de2b72351023dc073f3
Brad Murray <bdmurray@users.noreply.github.com>
2021-07-03 15:25:48 +0000

    more cleanup, centralizing data files and secrets

--- youtube/constants.py
+++ youtube/constants.py
@@ -1,5 +1,6 @@
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
-YT_CLIENT_SECRET = 'youtube/client_secret.json',
+YT_CLIENT_SECRET = 'data/client_secret.json',
 YT_MANAGE_SCOPE = 'https://www.googleapis.com/auth/youtube'
 YT_LIST_RETURN = "status, contentDetails"
+YT_AUTH_FILE = 'data/yt_auth_token.pickle'
--- youtube/helpers.py
+++ youtube/helpers.py
@@ -8,19 +8,19 @@ def get_credentials():
 
     credentials = None
 
-    # token.pickle stores the user's credentials from previously successful logins
-    if os.path.exists('token.pickle'):
-        print('Loading Credentials From File...')
-        with open('token.pickle', 'rb') as token:
+    # pickle file stores the user's credentials from previously successful logins
+    if os.path.exists(const.YT_AUTH_FILE):
+        #load credentials from file
+        with open(const.YT_AUTH_FILE, 'rb') as token:
             credentials = pickle.load(token)
 
-    # If there are no valid credentials available, then either refresh the token or log in.
+    # if there are no valid credentials available, then either refresh the token or log in.
     if not credentials or not credentials.valid:
         if credentials and credentials.expired and credentials.refresh_token:
-            print('Refreshing Access Token...')
+            # refreshing access token...
             credentials.refresh(Request())
         else:
-            print('Fetching New Tokens...')
+            # fetching new tokens
             flow = InstalledAppFlow.from_client_secrets_file(
                 const.YT_CLIENT_SECRET, scopes=[const.YT_MANAGE_SCOPE]
             )
@@ -28,9 +28,8 @@ def get_credentials():
             flow.run_local_server(port=8080, prompt='consent', authorization_prompt_message='')
             credentials = flow.credentials
 
-            # Save the credentials for the next run
-            with open('token.pickle', 'wb') as f:
-                print('Saving Credentials for Future Use...')
+            # save the credentials for the next run
+            with open(const.YT_AUTH_FILE, 'wb') as f:
                 pickle.dump(credentials, f)
 
     return credentials
